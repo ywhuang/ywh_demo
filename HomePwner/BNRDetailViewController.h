9d6d882c34ee52bc1c0e3a0b36146c55e76b88f7
Yiwei Huang <YiweiHuang@ywh-mac.var.ubisoft.org>
2016-01-22 17:49:59 -0500

    Initial Commit

---
+++ HomePwner/BNRDetailViewController.h
@@ -0,0 +1,18 @@
+//
+//  BNRDetailViewController.h
+//  HomePwner
+//
+//  Created by Yiwei Huang on 1/19/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRItem;
+
+@interface BNRDetailViewController : UIViewController
+
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end
---
+++ HomePwner/BNRDetailViewController.m
@@ -0,0 +1,81 @@
+//
+//  BNRDetailViewController.m
+//  HomePwner
+//
+//  Created by Yiwei Huang on 1/19/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+#import "BNRItem.h"
+
+@interface BNRDetailViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
+
+@implementation BNRDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    BNRItem * item = self.item;
+    
+    self.nameField.text = item.itemName;
+    self.serialNumberField.text = item.serialNumber;
+    self.valueField.text = [NSString stringWithFormat:@"%d", item.valueInDollars];
+    
+    static NSDateFormatter * dateFormatter = nil;
+    if (!dateFormatter) {
+        dateFormatter = [[NSDateFormatter alloc]init];
+        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    }
+    
+    self.dateLabel.text = [dateFormatter stringFromDate:item.dateCreated];
+}
+
+// Triggered when the "BACK" button of the navigation bar is tapped
+- (void)viewWillDisappear:(BOOL)animated {
+    
+    //remember to call super
+    [super viewWillDisappear:animated];
+    
+    //to dismiss keyboard
+    [self.view endEditing:YES];
+    
+    
+    BNRItem  * item = self.item;
+    item.itemName = self.nameField.text;
+    item.serialNumber = self.serialNumberField.text;
+    item.valueInDollars = [self.valueField.text intValue];
+    
+    
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
